d15ae5023a185d235a14a140af179c9ba0cf7030
ebonstar <18689574+ebonstar@users.noreply.github.com>
2023-01-25 04:36:15 +0000

    add main page blocks

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,47 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <div class="grid">
+      <div class="search">search</div>
+      <div class="one">one</div>
+      <div class="two">two</div>
+    </div>
   </main>
 </template>
+
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 100px 1fr 1fr;
+  gap: 0px 0px;
+  grid-template-areas:
+    "search"
+    "one"
+    "two";
+}
+
+.search {
+  grid-area: search;
+}
+
+.one {
+  grid-area: one;
+}
+
+.two {
+  grid-area: two;
+}
+
+@media (min-width: 1024px) {
+  .grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 100px 1fr;
+    gap: 1rem 1rem;
+    grid-auto-flow: row;
+    grid-template-areas:
+      "search search"
+      "one two";
+  }
+}
+</style>
